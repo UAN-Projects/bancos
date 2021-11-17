fb280142d43df014ee833a86b816511dfe1025fa
Edvaldo Valente <edvaldo.valente@ideiasdinamicas.com>
2021-11-17 08:30:14 +0100

    server done

--- server.php
+++ server.php
@@ -2,28 +2,31 @@
     require('app/libraries/nusoap/nusoap.php');
     
     function conexao() {
-        return new PDO('mysql:host=127.0.0.1:3307;dbname=larga_caixa', "edvaldo", "vander");
+        return new PDO('mysql:host=127.0.0.1:3307;dbname=bancos', "edvaldo", "vander");
     }
 
     function movimentar($token, $origem, $destino, $valor) {
         $conn = conexao();
         $return = 0;
+        $return = 555;
         $access = $conn->prepare("SELECT code_access.token FROM code_access WHERE code_access.token = :token")->execute(array('token' => $token))->fetch();
-        if($access) {
-            $conta_origem = $conn->prepare("SELECT * FROM contas WHERE contas.conta = :origem")->execute(array('origem' => $origem))->fetch();
-            $valor_actual = $conta_origem['valor'] - $valor;
+        if(!$access) {
+            // $return = $access->code;
+            $return = 777;
+            // $conta_origem = $conn->prepare("SELECT * FROM contas WHERE contas.conta = :origem")->execute(array('origem' => $origem))->fetch();
+            // $valor_actual = $conta_origem['valor'] - $valor;
 
-            if($valor_actual >= 0) {
-                $debito = $conn->prepare("UPDATE contas SET contas.valor = :valor WHERE contas.conta = :origem")->execute(array('valor' => $valor_actual, 'origem' => $origem));
-                if($debito) {
-                    $deposito = $conn->prepare("UPDATE contas SET contas.valor = :valor WHERE contas.conta = :destino")->execute(array('valor' => $valor, 'destino' => $destino));
-                    if($deposito) {
-                        $conta_origem = $conn->prepare("INSERT INTO movimentos (conta_origem, conta_destino, valor) VALUES (':conta_origem', ':conta_destino', ':valor')")
-                        ->execute(array('conta_origem' => $origem, 'conta_destino' => $destino, 'valor' => $valor));
-                        return $conn->lastInsertId();
-                    }
-                }
-            } 
+            // if($valor_actual >= 0) {
+            //     $debito = $conn->prepare("UPDATE contas SET contas.valor = :valor WHERE contas.conta = :origem")->execute(array('valor' => $valor_actual, 'origem' => $origem));
+            //     if($debito) {
+            //         $deposito = $conn->prepare("UPDATE contas SET contas.valor = :valor WHERE contas.conta = :destino")->execute(array('valor' => $valor, 'destino' => $destino));
+            //         if($deposito) {
+            //             $conta_origem = $conn->prepare("INSERT INTO movimentos (conta_origem, conta_destino, valor) VALUES (':conta_origem', ':conta_destino', ':valor')")
+            //             ->execute(array('conta_origem' => $origem, 'conta_destino' => $destino, 'valor' => $valor));
+            //             return $conn->lastInsertId();
+            //         }
+            //     }
+            // } 
         }
         
         return $return;
@@ -32,11 +35,11 @@
     function getToken($code) {
         $conn = conexao();
         $stmt = $conn->prepare(
-          "SELECT code_access.token FROM code_access WHERE code_access.code = :code ");
+          "SELECT code_access.token FROM code_access WHERE code_access.code = :code");
         $stmt->execute(array('code' => $code));
 
-        $user = $stmt->fetch();
-        return ($user) ? $user['code'] : '';
+        $token = $stmt->fetch();
+        return ($token) ? $token['token'] : '';
     }
 
     $server = new soap_server();
@@ -69,5 +72,4 @@
     );
 
     $server->service(file_get_contents("php://input"));
-
 ?>
